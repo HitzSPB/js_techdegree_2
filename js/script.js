698c54172974d2bb83129570142f4fdbfadf04da
HitzSPB <daniel.oesterbro@gmail.com>
2021-01-24 14:33:31 +0100

    added last bit of code for search button

--- js/script.js
+++ js/script.js
@@ -4,10 +4,7 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 let studentPerPage = 9;
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+// ShowPage function that handles the student data showing on the page
 function showPage(list, page) {
    function studentItem(studentInformation) {
       let li = document.createElement('li');
@@ -46,21 +43,17 @@ function showPage(list, page) {
       return li;
    }
 
-   // create two variables which will represent the index for the first and last student on the page
+   // Handling the indexing of the page
    let startIndex = (page * studentPerPage) - studentPerPage;
    let endIndex = page * studentPerPage;
 
-   // select the element with a class of `student-list` and assign it to a variable
+   // selecting list for student elements
    let studentListUl = document.querySelector(".student-list");
 
-   // set the innerHTML property of the variable you just created to an empty string
+   // Clear former data from Student UL
    studentListUl.innerHTML = "";
 
-   // loop over the length of the `list` parameter
-   // inside the loop create a conditional to display the proper students
-   // inside the conditional:
-   // create the elements needed to display the student information
-   // insert the above elements
+   // Looping through the students and inserting the correct range
    for (let index = 0; index < list.length; index++) {
       if (index >= startIndex && index < endIndex) {
          studentListUl.appendChild(studentItem(list[index]));
@@ -69,33 +62,87 @@ function showPage(list, page) {
 }
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+// Function to handle creating the correct amount of pages
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
-   let numOfPages = Math.ceil(data.length / studentPerPage);
+   function createPage(number) {
+      let li = document.createElement('li');
+      let button = document.createElement('button');
+      button.type = "button";
+      button.textContent = number;
 
-   // select the element with a class of `link-list` and assign it to a variable
-   let linkListUl = document.querySelector("link-list");
+      li.appendChild(button);
+      return li;
+   }
+   // Calculating the correct amount of pages from the list data
+   let numOfPages = Math.ceil(list.length / studentPerPage);
 
-   // set the innerHTML property of the variable you just created to an empty string
+   let linkListUl = document.querySelector(".link-list");
+
+   // clearing the pages when new data inserted
    linkListUl.innerHTML = "";
-   
-   // loop over the number of pages needed
-   // create the elements needed to display the pagination button
-   // insert the above elements
 
-   // give the first pagination button a class of "active"
-
-   // create an event listener on the `link-list` element
-   // if the click target is a button:
-   // remove the "active" class from the previous button
-   // add the active class to the clicked button
-   // call the showPage function passing the `list` parameter and page to display as arguments
+   // Checking the list is not empty
+   if (list.length != 0) {
+      for (let index = 0; index < numOfPages; index++) {
+         linkListUl.appendChild(createPage(index + 1));
+      }
+      // give the first pagination button a class of "active"
+      linkListUl.firstChild.firstChild.className = "active";
+      console.log(linkListUl.firstChild);
+      // create an event listener on the `link-list` element
+      // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+      linkListUl.addEventListener('click', (e) => {
+         if (e.target.tagName === "BUTTON") {
+            linkListUl.querySelector(".active").classList.remove("active");
+            e.target.className = "active";
+            showPage(list, e.target.textContent);
+         }
+      });
+   }
+   else {
+      // If the list is empty we add "No results instead of buttons"
+      linkListUl.innerHTML = "No results";
+      showPage(list, 0);
+   }
 }
 
+// Search bar
+function searchBar(list) {
+   // Using template literal instead of creating elements like previously
+   let searchBarHtml = `<label for="search" class="student-search">
+         <input id="search" placeholder="Search by name...">
+            <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+          </label>`;
+   let pageHeader = document.querySelector("header");
+   pageHeader.insertAdjacentHTML('beforeend', searchBarHtml);
+
+   let searchSubmitButton = document.querySelector('header button');
+
+   // Adding event listener for search button and looping through first and last names to see if it matches the search word
+   searchSubmitButton.addEventListener('click', (e) => {
+      if (e.target.tagName === "BUTTON") {
+         let input = document.querySelector('#search')
+         const text = input.value.toLowerCase();
+         input.value = '';
+
+         let newList = [];
+         for (let index = 0; index < list.length; index++) {
+            if (list[index].name.first.toLowerCase().includes(text) || list[index].name.last.toLowerCase().includes(text)) {
+               console.log(list[index].name.first + " " + list[index].name.last)
+               newList.push(list[index]);
+            }
+         }
+         showPage(newList, 1);
+         addPagination(newList);
+      }
+   })
+
+}
 
 // Call functions
-showPage(data, 2);
+showPage(data, 1);
+addPagination(data);
+searchBar(data);
