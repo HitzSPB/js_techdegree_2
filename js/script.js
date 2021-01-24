e9698e1cd27a5e9f149e3eb5b1a9b98a21aac714
HitzSPB <daniel.oesterbro@gmail.com>
2021-01-24 10:17:04 +0100

    Added project files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+function showPage(list, page) {
+   // create two variables which will represent the index for the first and last student on the page
+   // let firstStudent = list.
+   // select the element with a class of `student-list` and assign it to a variable
+
+   // set the innerHTML property of the variable you just created to an empty string
+
+   // loop over the length of the `list` parameter
+   // inside the loop create a conditional to display the proper students
+   // inside the conditional:
+   // create the elements needed to display the student information
+   // insert the above elements
+}
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
